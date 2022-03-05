4477a52439140955da9efe5341652947805a71f5
SJRios07 <santiago.jimenez_rio@uao.edu.co>
2022-03-04 20:29:17 -0500

    update

---
+++ Unity UAO/Assets/SantiagoJ/Scripts_SJ/Script_Torreta_SJ.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Torreta_SJ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
